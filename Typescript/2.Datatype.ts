6d9a7c613bfcfc753356d45e557264df9f1bf666
Akshadachobhee <102922884+Akshadachobhee@users.noreply.github.com>
2023-03-17 00:30:39 +0530

    Delete 2.Datatype.ts

--- Typescript/2.Datatype.ts
+++
@@ -1,7 +0,0 @@
-var No1 : number =11 // standard syntax to create variable
-console.log(No1)
-// Variations to create variable 
-var No2 =11 
-var No3 
-var No4 : number
-// number - In typescript number is datatype which can store int, float , double values like in C 
